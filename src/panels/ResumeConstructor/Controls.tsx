f8af805187d38d033a46e17c098cbb7fcc81bd10
Dmitry Smotryaev <dmitry.smotryaev@gmail.com>
2025-05-01 04:01:20 +0300

    [STABLE] Исправил баг с бесконечными шагами формы

--- src/panels/ResumeConstructor/Controls.tsx
+++ src/panels/ResumeConstructor/Controls.tsx
@@ -4,10 +4,14 @@ import { FC } from 'react'
 export interface ControldsProps {
   handlePrevStep: () => void,
   handleNextStep: () => void,
+  handleFinalStep: () => void,
   step: number,
+  maxSteps: number,
 }
 
-export const Controls: FC<ControldsProps> = ({ handlePrevStep, handleNextStep, step }) => (
+export const Controls: FC<ControldsProps> = ({
+  handlePrevStep, handleNextStep, handleFinalStep, step, maxSteps,
+}) => (
   <ButtonGroup stretched mode="horizontal" align="center">
     {step > 0 && (
       <FormItem style={{ width: '100%', boxSizing: 'border-box' }}>
@@ -17,9 +21,9 @@ export const Controls: FC<ControldsProps> = ({ handlePrevStep, handleNextStep, s
       </FormItem>
     )}
 
-    {step === 5 ? (
+    {step === maxSteps ? (
       <FormItem style={{ width: '100%', boxSizing: 'border-box' }}>
-        <Button stretched size="l" onClick={handleNextStep}>
+        <Button stretched size="l" onClick={maxSteps ? handleFinalStep : handleNextStep}>
           Завершить
         </Button>
       </FormItem>
